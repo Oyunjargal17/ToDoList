36759ade6b4b9137e3f49f5d4f27318a67db761e
Oyunjargal17 <oyunjargal.teacher@gmail.com>
2025-12-08 20:21:28 +0800

    12_08 gert

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector("#input");
 const addElement = document.querySelector("#add");
+const tasksContainer= document.querySelector("#taskContainer")
 console.log(input);
 console.log(addElement);
 
@@ -17,7 +18,24 @@ const add = () => {
   taskId++;
   console.log(tasks);
   clearInput();
+  renderTasks();
 };
+const renderTasks= () =>
+{
+        tasks.forEach((task)=>
+        {
+          const taskElement = createTaskElement(task);
+          tasksContainer.innerHTML= taskElement;
+        })
+}
+const createTask= (task)=>
+{
+  return `<div class="task">
+          <input type="checkbox" class="task__checkbox" name="checkbox">
+          <p class="task__text">${task.text}</p>
+          <button class="task__delete">delete</button>
+        </div>`
+}
 const clearInput = () => {
   input.value = "";
 };
